b2f3932fb7be13dcf1d617a1f329a7dad5287540
kyoongdev <9898junjun2@gmail.com>
2022-10-29 21:52:55 +0900

    ADD : Section4 Chapter6 functions

--- section4/ch6/src/controllers/users/index.js
+++ section4/ch6/src/controllers/users/index.js
@@ -10,10 +10,42 @@ class UserController {
 
   #init() {
     this.router.get("/", this.#getUsers);
+    this.router.post("/", this.#createUser);
+    this.router.patch("/:id", this.#updateUser);
+    this.router.delete("/:id", this.#deleteUser);
   }
 
   #getUsers(req, res) {
     res.status(200).json({ users });
+  }
+
+  #createUser(req, res) {
+    const { name, age } = req.body;
+    const id = new Date().getTime();
+
+    users.push({ id, name, age });
+
+    res.status(201).json({ id });
+  }
+
+  #updateUser(req, res) {
+    const { id } = req.params;
+    const { name, age } = req.body;
+
+    users.forEach((user, index) => {
+      if (user.id === Number(id)) {
+        users[index] = { ...user, name, age };
+      }
+    });
+
+    res.status(204).json({});
+  }
+
+  #deleteUser(req, res) {
+    const { id } = req.params;
+    users = users.filter((user) => user.id !== Number(id));
+
+    res.status(204).json({});
   }
 }
 
--- section4/ch6/src/index.js
+++ section4/ch6/src/index.js
@@ -14,6 +14,7 @@ app.use(
   })
 );
 app.use(helmet());
+
 controllers.forEach((controller) => {
   app.use(controller.path, controller.router);
 });
